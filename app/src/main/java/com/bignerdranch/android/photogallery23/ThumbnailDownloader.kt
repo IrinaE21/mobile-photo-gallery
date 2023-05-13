a25e9572831ee88afeb5a23729f9684a4d696304
IES19991021 <erina.1r@yandex.ru>
2023-05-13 13:33:32 +0700

    Шаг 25.13. Добавление констант и переменных и Шаг 25.14 Отправка сообщения

--- app/src/main/java/com/bignerdranch/android/photogallery23/ThumbnailDownloader.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery23/ThumbnailDownloader.kt
@@ -1,15 +1,22 @@
 package com.bignerdranch.android.photogallery23
 
+import android.os.Handler
 import android.os.HandlerThread
 import android.util.Log
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleObserver
 import androidx.lifecycle.OnLifecycleEvent
+import java.util.concurrent.ConcurrentHashMap
 
 private const val TAG = "ThumbnailDownloader"
+private const val MESSAGE_DOWNLOAD = 0
+
 
 class ThumbnailDownloader<in T>: HandlerThread(TAG), LifecycleObserver {
     private var hasQuit = false
+    private lateinit var requestHandler: Handler
+    private val requestMap = ConcurrentHashMap<T, String>()
+    private val flickrFetchr = FlickrFetchr()
 
     override fun quit(): Boolean {
         hasQuit = true
@@ -32,5 +39,9 @@ class ThumbnailDownloader<in T>: HandlerThread(TAG), LifecycleObserver {
     fun queueThumbnail(target: T, url: String)
     {
         Log.i(TAG, "Got a URL: $url")
+
+        requestMap[target] = url
+        requestHandler.obtainMessage(MESSAGE_DOWNLOAD, target).sendToTarget()
+
     }
 }
